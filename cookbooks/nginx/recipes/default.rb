84ca34d1bf7a2ec3123af3fb2346bb4a66a3c928
ac <root@localhost>
2017-08-20 18:14:12 -0400

    added new cookbooks

---
+++ cookbooks/nginx/recipes/default.rb
@@ -0,0 +1,9 @@
+#
+# Cookbook:: nginx
+# Recipe:: default
+#
+# Copyright:: 2017, The Authors, All Rights Reserved.
+
+package 'nginx' do
+  action :install
+end
---
+++ cookbooks/vim/recipes/default.rb
@@ -0,0 +1,8 @@
+#
+# Cookbook:: vim
+# Recipe:: default
+#
+# Copyright:: 2017, The Authors, All Rights Reserved.
+package 'vim' do
+  action :install
+end
